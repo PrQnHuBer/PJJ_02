e1c8f9937c8fc7de83295cb05b097b8dfce2cd0e
Dumbhag <s6519410057@sau.ac.th>
2023-09-08 09:27:14 +0700

    v.1

---
+++ test01.py
@@ -0,0 +1,6 @@
+print("hello")
+print(23235)
+print(True)
+print(False)
+print(456 + 456)
+print("AAA\nBBB\t\tCCC\nDEF\bXYZ")
---
+++ test02.py
@@ -0,0 +1,6 @@
+print("hello",456, True,10+20)
+print("hello"+str(456)+ str(True) +str(10+20))
+print('Hello{}ok'',format(456,True ,10+20)')
+print('Hello{0}ok'',format(456,True ,10+20)')
+print('hello {456} ok {True}{10+20}')
+print('hello %d ok %s no %d' )
